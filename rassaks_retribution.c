3cee805f7d990c31d8bcac322c366f2c4ae253f0
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-09-13 18:37:38 -0300

    Configure build.

---
+++ rassaks_retribution.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lib/SMSlib.h"
+#include "lib/PSGlib.h"
+#include "data.h"
+
+void main() {
+	SMS_useFirstHalfTilesforSprites(1);
+	SMS_setSpriteMode(SPRITEMODE_TALL);
+	SMS_VDPturnOnFeature(VDPFEATURE_HIDEFIRSTCOL);
+	
+	while (1) {
+	}
+}
+
+SMS_EMBED_SEGA_ROM_HEADER(9999,0); // code 9999 hopefully free, here this means 'homebrew'
+SMS_EMBED_SDSC_HEADER(0,1, 2021,9,13, "Haroldo-OK\\2021", "Rassak's Retribution",
+  "An Yar's Revenge clone.\n"
+  "Built using devkitSMS & SMSlib - https://github.com/sverx/devkitSMS");
