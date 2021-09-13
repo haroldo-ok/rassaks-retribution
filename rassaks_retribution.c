48c2f13b270f50be8cfce0f5972508abd9f35635
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-09-13 18:52:03 -0300

    Load player sprite tiles into VRAM.

--- rassaks_retribution.c
+++ rassaks_retribution.c
@@ -5,10 +5,21 @@
 #include "lib/PSGlib.h"
 #include "data.h"
 
+void load_standard_palettes() {
+	SMS_loadBGPalette(sprites_palette_bin);
+	SMS_loadSpritePalette(sprites_palette_bin);
+}
+
 void main() {
 	SMS_useFirstHalfTilesforSprites(1);
 	SMS_setSpriteMode(SPRITEMODE_TALL);
 	SMS_VDPturnOnFeature(VDPFEATURE_HIDEFIRSTCOL);
+
+	SMS_displayOff();
+	SMS_loadPSGaidencompressedTiles(sprites_tiles_psgcompr, 0);
+	load_standard_palettes();
+
+	SMS_displayOn();
 	
 	while (1) {
 	}
